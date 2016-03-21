19bf3787bb8264e50a75194268c94c91e604b671
John Young <u1071673@umail.com>
2016-03-21 15:22:44 -0600

    Added timer in view

--- Boggle/Boggle/BoggleGUI.cs
+++ Boggle/Boggle/BoggleGUI.cs
@@ -12,12 +12,36 @@ namespace Boggle
 {
     //View Class
 
+
     public partial class BoggleGUI : Form
     {
+        //Properties to comunicate with the server
+        public int Time { get; set; }
+        public string word { get; set; }
+        public string Status { get; set; }
+        public string Letter { get; set; }
+
+
+
         public BoggleGUI()
         {
             InitializeComponent();
         }
+        private void JoinButton_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            TimeBox.Text = Time.ToString();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            TimeBox.Text = Time.ToString();
+            if (Time == 0)
+            {
+                timer.Stop();
+            }
+        }
+
+
 
     }
 }
