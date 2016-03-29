951ba75bccdf20657b92f5297d93d1b54bb8f80c
John Young <u1071673@umail.com>
2016-03-29 15:14:31 -0600

    Fixed missing }.

--- BoggleService/BoggleService/DataModel.cs
+++ BoggleService/BoggleService/DataModel.cs
@@ -80,61 +80,56 @@ namespace Boggle
             {
                 return 0;
             }
+        }
     }
+        [DataContract]
+        public class Words
+        {
+            [DataMember(EmitDefaultValue = false)]
+            public string Word { get; set; }
 
-    [DataContract]
-    public class Words 
-    {
-        [DataMember(EmitDefaultValue = false)]
-        public string Word { get; set; }
+            [DataMember(EmitDefaultValue = false)]
+            public int? Score { get; set; }
 
-        [DataMember(EmitDefaultValue = false)]
-        public int? Score { get; set; }
+        }
+        [DataContract]
+        public class UserInfo
+        {
+            [DataMember(EmitDefaultValue = false)]
+            public string Nickname { get; set; }
+        }
+        [DataContract]
+        public class CreateUserReturn
+        {
+            [DataMember(EmitDefaultValue = false)]
+            public string UserToken { get; set; }
+        }
+        [DataContract]
+        public class JoinGameReturn
+        {
+            [DataMember(EmitDefaultValue = false)]
+            public string GameID { get; set; }
+        }
+        [DataContract]
+        public class PlayWordReturn
+        {
+            [DataMember(EmitDefaultValue = false)]
+            public int Score { get; set; }
+        }
 
-    }
+        public class JoinGameArgs
+        {
+            public string UserToken { get; set; }
 
-    [DataContract]
-    public class UserInfo
-    {
-        [DataMember(EmitDefaultValue = false)]
-        public string Nickname { get; set; }
-    }
-
-    [DataContract]
-    public class CreateUserReturn
-    {
-        [DataMember(EmitDefaultValue = false)]
-        public string UserToken { get; set; }
-    }
-
-    [DataContract]
-    public class JoinGameReturn
-    {
-        [DataMember(EmitDefaultValue = false)]
-        public string GameID { get; set; }
-    }
-    [DataContract]
-    public class PlayWordReturn
-    {
-        [DataMember(EmitDefaultValue = false)]
-        public int Score { get; set; }
-    }
-
-    public class JoinGameArgs
-    {
-        public string UserToken { get; set; }
-
-        public int TimeLimit { get; set; }
-    }
-
-    public class CancelGameArgs
-    {
-        public string UserToken { get; set; }
-    }
-    public class PlayWordArgs
-    {
-        public string UserToken { get; set; }
-        public string Word { get; set; }
-    }
+            public int TimeLimit { get; set; }
+        }
+        public class CancelGameArgs
+        {
+            public string UserToken { get; set; }
+        }
+        public class PlayWordArgs
+        {
+            public string UserToken { get; set; }
+            public string Word { get; set; }
+        }
 }
-
